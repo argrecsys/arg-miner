b57e2c4f2984e4d4f1511cf26f50a4947d342ab0
Andres Segura <seguraandres7@gmail.com>
2021-07-14 17:15:27 -0500

    Update ArgumentMiner solution
    Updated data quality command.

--- sql/data-quality.sql
+++ sql/data-quality.sql
@@ -2,14 +2,21 @@ USE `decide.madrid_2019_09`;
 
 SELECT id, url, code, title, userId, date, summary, text, numComments, status, numSupports, isAssociation
   FROM proposals
- WHERE id IN (15707, 23248);
+ WHERE id IN (4696, 15707, 23248);
 
--- El bus 5 pasa por general martinez campos pero no para ayi
+-- title: Iluminacion de pasos de cebra
+-- summary: Se ha vuelto un peligro cruzar en algunos pasos de cebra, debido a la baja iluminacion que tienen.
+UPDATE proposals
+   SET title = 'Iluminación de pasos de cebra'
+     , summary = 'Se ha vuelto un peligro cruzar en algunos pasos de cebra, debido a la baja iluminación que tienen.'
+ WHERE id = 4696;
+
+-- summary: El bus 5 pasa por general martinez campos pero no para ayi
 UPDATE proposals
    SET summary = 'El bus 5 pasa por general martinez campos pero no para allí.'
  WHERE id = 15707;
 
--- En Carabanchel han aumentado los delitos por robo en personas y viviendas.sin embargo la dotacion policial para el distrito se ha disminuido.
+-- summary: En Carabanchel han aumentado los delitos por robo en personas y viviendas.sin embargo la dotacion policial para el distrito se ha disminuido.
 UPDATE proposals
    SET summary = 'En Carabanchel han aumentado los delitos por robo en personas y viviendas, sin embargo la dotacion policial para el distrito se ha disminuido.'
  WHERE id = 23248;
