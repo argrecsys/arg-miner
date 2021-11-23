ef97b49ee53e968ac67d45fb3afaffdbd159d561
Andres Segura-Tinoco <seguraandres7@gmail.com>
2021-11-23 16:48:43 +0100

    Update ArgMiner
    Minor changes.

--- queries/mysql-comments-queries.sql
+++ queries/mysql-comments-queries.sql
@@ -1,14 +1,14 @@
 USE `decide.madrid_2019_09`;
 
 -- Grouping comments by proposalsId
-SELECT proposalId, COUNT(*)
+SELECT proposalId, COUNT(*) AS "count"
   FROM proposal_comments
- WHERE proposalId IN (23219, 7, 1419)
+ WHERE proposalId IN (23219, 7, 1419, 64, 15711, 59, 1085, 2145, 3206, 18750)
  GROUP BY proposalId
- ORDER BY proposalId, parentId;
+ ORDER BY count DESC, proposalId;
  
 -- Search comments by proposalsId
 SELECT id, proposalId, parentId, userId, text
   FROM proposal_comments
- WHERE proposalId IN (23219, 7, 1419)
+ WHERE proposalId IN (2145)
  ORDER BY proposalId, parentId;
